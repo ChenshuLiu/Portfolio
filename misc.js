b11c8a4aecc3a56f3ff2442ba9b119f8dbb96a39
PinyiYang <chenshu0615@g.ucla.edu>
2025-05-18 19:57:25 +0800

    update header opions

--- misc.js
+++ misc.js
@@ -21,3 +21,10 @@ window.onclick = function (event) {
 };
 });
 
+const menuToggle = document.getElementById("menu-toggle");
+const navLinks = document.querySelector(".nav-links");
+
+menuToggle.addEventListener("click", () => {
+  navLinks.classList.toggle("active");
+});
+  
--- research.js
+++ research.js
@@ -71,3 +71,10 @@
     });
   })();
   
+  const menuToggle = document.getElementById("menu-toggle");
+const navLinks = document.querySelector(".nav-links");
+
+menuToggle.addEventListener("click", () => {
+  navLinks.classList.toggle("active");
+});
+  
